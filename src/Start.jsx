b2fd93f091315892fa92a86924d2091302de0884
Decoldest <127795154+Decoldest@users.noreply.github.com>
2024-03-27 17:08:45 -0400

    add disney image

--- src/Start.jsx
+++ src/Start.jsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import fry from "./assets/fry.png";
 import zoidberg from "./assets/zoidberg.png";
 import leela from "./assets/leela.png";
+import disney from "./assets/Disney.png"
 
 Start.propTypes = {
   setDifficulty: PropTypes.func,
@@ -15,22 +16,25 @@ export default function Start({ setDifficulty, setIsPlaying }) {
   };
 
   return (
-    <div className="start-container">
-      <h2>Select Each Character Once. Choose difficulty:</h2>
-      <div className="difficulty-buttons">
-        <button onClick={() => setDifficultyAndStart(5)}>
-          <img src={fry} alt="Philip J Fry" />
-          Easy
-        </button>
-        <button onClick={() => setDifficultyAndStart(10)}>
-          <img src={zoidberg} alt="Doctor Zoidberg" />
-          Medium
-        </button>
-        <button onClick={() => setDifficultyAndStart(20)}>
-          <img src={leela} alt="Doctor Zoidberg" />
-          Hard
-        </button>
+    <>
+      <div className="start-container">
+        <h2>Select Each Character Once. Choose difficulty:</h2>
+        <div className="difficulty-buttons">
+          <button onClick={() => setDifficultyAndStart(5)}>
+            <img src={fry} alt="Philip J Fry" />
+            Easy
+          </button>
+          <button onClick={() => setDifficultyAndStart(10)}>
+            <img src={zoidberg} alt="Doctor Zoidberg" />
+            Medium
+          </button>
+          <button onClick={() => setDifficultyAndStart(20)}>
+            <img src={leela} alt="Doctor Zoidberg" />
+            Hard
+          </button>
+        </div>
       </div>
-    </div>
+      <img src={disney} alt="Disney Futurama" className="disney" />
+    </>
   );
 }
